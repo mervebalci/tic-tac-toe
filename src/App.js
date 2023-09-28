fd3ee86dc49d5115197614e542bfe88f46b445e5
Merve Balci <mervebalci090@gmail.com>
2023-09-27 22:46:50 -0300

    Building the board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import './App.css';
 
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  )
 }
+
+// className="square" is a button property or prop that tells CSS how to style the button.
+// Group the squares into rows with divs and add some CSS classes(board-row) to make a 3x3 board.
